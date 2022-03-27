0f9a24bd37e9820cf26ecd109213b66997fed89e
roa <84313163+roacn@users.noreply.github.com>
2022-03-27 23:47:03 +0800

    .

--- build/Lienol_source/diy-part.sh
+++ build/Lienol_source/diy-part.sh
@@ -5,11 +5,11 @@ echo "--------------diy-part start--------------"
 echo
 echo '修改 IP设置'
 cat >$NETIP <<-EOF
-uci set network.lan.ipaddr='192.168.2.2'                                    # IPv4 地址(openwrt后台地址)
+uci set network.lan.ipaddr='192.168.1.2'                                    # IPv4 地址(openwrt后台地址)
 uci set network.lan.netmask='255.255.255.0'                                 # IPv4 子网掩码
-uci set network.lan.gateway='192.168.2.1'                                   # IPv4 网关
-uci set network.lan.broadcast='192.168.2.255'                               # IPv4 广播
-uci set network.lan.dns='223.5.5.5 114.114.114.114'                         # DNS(多个DNS要用空格分开)
+uci set network.lan.gateway='192.168.1.1'                                   # IPv4 网关
+uci set network.lan.broadcast='192.168.1.255'                               # IPv4 广播
+uci set network.lan.dns='223.5.5.5 210.2.4.8'                               # DNS(多个DNS要用空格分开)
 uci set network.lan.delegate='0'                                            # 去掉LAN口使用内置的 IPv6 管理
 uci commit network                                                          # 不要删除跟注释,除非上面全部删除或注释掉了
 #uci set dhcp.lan.ignore='1'                                                 # 关闭DHCP功能（去掉uci前面的#生效）
